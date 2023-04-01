77c8e4019beeb21f88ecf58129c354ed52a31d35
weu-souza <weuller1561@gmail.com>
2023-04-01 17:47:27 -0300

    validando formulario e arrumando o estilo da tabela

--- aula1/js/script.js
+++ aula1/js/script.js
@@ -10,51 +10,76 @@ function enviar() {
     //pega os valores do select com o id modelo-veiculo
     let marca = document.getElementById('marcas-veiculo').value
     //cria a variavel do input com o atributo "nome = ja-batido"
-    let jaBatido = document.getElementsByName("ja-batido")
+    let jaBatido = document.getElementsByName("batido")
     //pega os valores do input com o id ja batido e pega o valor do elemento selecionado
     let escolha = []
+
     for (let i = 0; i < jaBatido.length; i++) {
         if (jaBatido[i].checked) {
-            escolha.push(jaBatido[i].value)
+            escolha = jaBatido[i].value
         }
     }
 
-    // guarda um array do formulario
+    if (validar()){
+        alert("algum campo ficou vazio ou incorreto,preencha o formulario!!")
+    }
+    else {
+        tabela(modelo, cor, ano, nomePropietario, marca, escolha)
+    }
 
-    tabela(modelo, cor, ano, nomePropietario, marca, escolha)
 
-   
 }
-
 
 function tabela(modelo, cor, ano, nomePropietario, marca, escolha) {
     // guarda o valor pego no formulario em um array
     let guardarValor = [modelo, cor, marca, ano, escolha, nomePropietario]
+    criarTabela(guardarValor)
 
-    // insere os valores do array na tabela
-    for (let i = 0; i < guardarValor.length; i++) {
-        // pega a tudo do documento que estiver com a classe tabela-tr
-        let tabelaTr = document.getElementsByClassName('tabela-tr')[i]
-        // cria um elemento td na tabela
-        let tdMod = document.createElement('td')
-        // guarda o valor inserido no array pelo input dentro do elemento td
-        tdMod.textContent = guardarValor[i]
-        // cria uma classe tabela-resposta no td(para poder inserir o css que criei com esse mesmo nome)
-        tdMod.className = "tabela-resposta"
-        // insere o td criado dentro do tr (basicamente fala que o td e o filho do tr e o td e pra ser inserido dentro do tr)
-        tabelaTr.appendChild(tdMod)
-    }
-    // cria uma variavel de acordo com o id do html
-    let count = document.getElementById('contar')
-    // soma o valor da coluna que inicialmente não existe (colspan e um atributo de td para ele ocupar o
-    // tanto de espaço que for inserido no colspan se for inserido 3, o td ira ocupar 3 espaços na tabela e assim por diante)
-    // como o total de veiculos cadastrados teria que estar no rodape, e ocupar a tabela inteira eu coloquei pra alterar o colspan junto no js
-    count.colSpan++
-    // soma +1 e altera o valor que inicialmente e zero
-    count.textContent++
-    // desativa o botão quando chegar em 20 veiculos cadastrados para a tabela não ficar feia
-    if (count.textContent>19){
-        let btn = document.getElementById('btn-enviar').disabled=true;
-    }
 
+
+// cria uma variavel de acordo com o id do html
+let count = document.getElementById('contar')
+// soma +1 e altera o valor que inicialmente e zero
+count.textContent++
+
+}
+function criarTabela(guardarValor){
+    let tBoddy = document.getElementById('table-boddy')
+    // pega a tudo do documento que estiver com a classe tabela-tr
+    let tabelaTr = document.createElement('tr')
+    // cria um elemento td na tabela
+    let tdMod = document.createElement('td')
+    let tdCor = document.createElement('td')
+    let tdMarca = document.createElement('td')
+    let tdAno = document.createElement('td')
+    let tdJaBatido = document.createElement('td')
+    let tdPropietario = document.createElement('td')
+
+
+    // guarda o valor inserido no array pelo input dentro do elemento td
+    tdMod.textContent = guardarValor[0]
+    tdCor.textContent = guardarValor[1]
+    tdMarca.textContent = guardarValor[2]
+    tdAno.textContent = guardarValor[3]
+    tdJaBatido.textContent = guardarValor[4]
+    tdPropietario.textContent = guardarValor[5]
+
+    // cria uma classe tabela-resposta no td(para poder inserir o css que criei com esse mesmo nome)
+    tdMod.className = "tabela-resposta"
+    tdCor.className = "tabela-resposta"
+    tdMarca.className = "tabela-resposta"
+    tdAno.className = "tabela-resposta"
+    tdJaBatido.className = "tabela-resposta"
+    tdPropietario.className = "tabela-resposta"
+    // insere o td criado dentro do tr (basicamente fala que o td e o filho do tr e o td e pra ser inserido dentro do tr)
+    tabelaTr.append(tdMod,tdCor,tdMarca,tdAno,tdJaBatido,tdPropietario)
+    tBoddy.appendChild(tabelaTr)
+}
+function validar(){
+   return !(document.formulario.modelo.value
+       && document.formulario.cor.value
+       && document.formulario.marcas.value
+       && document.formulario.ano.value
+       && document.formulario.batido.value
+       && document.formulario.proprietario.value);
 }
