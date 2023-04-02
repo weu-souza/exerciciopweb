a074b86ebc131eb9cc4ef512ae85108590ab5760
weu-souza <weuller1561@gmail.com>
2023-04-02 16:30:27 -0300

    objetos

--- aula1/js/script.js
+++ aula1/js/script.js
@@ -1,82 +1,68 @@
 function enviar() {
-    //pega os valores do input com o id modelo-veiculo
-    let modelo = document.getElementById("modelo-veiculo").value
-    //pega os valores do input com o id modelo-cor
-    let cor = document.getElementById("cor-veiculo").value
-    //pega os valores do input com o id modelo-ano
-    let ano = document.getElementById("ano-veiculo").value
-    //pega os valores do input com o id nome-proprietario
-    let nomePropietario = document.getElementById("nome-proprietario").value
-    //pega os valores do select com o id modelo-veiculo
-    let marca = document.getElementById('marcas-veiculo').value
-    //cria a variavel do input com o atributo "nome = ja-batido"
-    let jaBatido = document.getElementsByName("batido")
-    //pega os valores do input com o id ja batido e pega o valor do elemento selecionado
-    let escolha = []
-
-    for (let i = 0; i < jaBatido.length; i++) {
-        if (jaBatido[i].checked) {
-            escolha = jaBatido[i].value
-        }
+    const guardarValor = {modelo : document.getElementById("modelo-veiculo").value,
+        cor : document.getElementById("cor-veiculo").value,
+        ano : document.getElementById("ano-veiculo").value,
+        nomePropietario : document.getElementById("nome-proprietario").value,
+        marca : document.getElementById('marcas-veiculo').value,
+        jaBatido : document.querySelector("[name = 'batido']:checked").value
     }
-
     if (validar()) {
         alert("algum campo ficou vazio ou incorreto,preencha o formulario!!")
     } else {
-        tabela(modelo, cor, ano, nomePropietario, marca, escolha)
+        tabela(guardarValor)
     }
-
-
 }
 
-function tabela(modelo, cor, ano, nomePropietario, marca, escolha) {
-    // guarda o valor pego no formulario em um array
-    let guardarValor = [modelo, cor, marca, ano, escolha, nomePropietario]
+function tabela(guardarValor) {
     criarTabela(guardarValor)
-
-
 // cria uma variavel de acordo com o id do html
-    let count = document.getElementById('contar')
-// soma +1 e altera o valor que inicialmente e zero
+    const count = document.getElementById('contar')
+// soma 1 e altera o valor que inicialmente e zero
     count.textContent++
 //     cria o a variavel para o botão de excluir
-let btnApagar = document.getElementById('excluir-tabela')
+    const btnApagar = document.getElementById('excluir-tabela')
     // cria uma variavel pra mudar o display que inicialmente e none para inline
     // isso faz o botão que inicialmente e none aparecer na tela
     btnApagar.style.display = 'inline'
 }
 
 function criarTabela(guardarValor) {
-    let tBoddy = document.getElementById('table-boddy')
+    const tBoddy = document.getElementById('table-boddy')
     // pega a tudo do documento que estiver com a classe tabela-tr
-    let tabelaTr = document.createElement('tr')
+    const tabelaTr = document.createElement('tr')
     tabelaTr.className = 'tabela-tr'
     // cria um elemento td na tabela
-    let tdMod = document.createElement('td')
-    let tdCor = document.createElement('td')
-    let tdMarca = document.createElement('td')
-    let tdAno = document.createElement('td')
-    let tdJaBatido = document.createElement('td')
-    let tdPropietario = document.createElement('td')
+    const tabelaTd = {
+        tdMod : document.createElement('td'),
+         tdCor : document.createElement('td'),
+         tdMarca : document.createElement('td'),
+         tdAno : document.createElement('td'),
+         tdJaBatido : document.createElement('td'),
+         tdPropietario : document.createElement('td'),
+    }
+
 
 
     // guarda o valor inserido no array pelo input dentro do elemento td
-    tdMod.textContent = guardarValor[0]
-    tdCor.textContent = guardarValor[1]
-    tdMarca.textContent = guardarValor[2]
-    tdAno.textContent = guardarValor[3]
-    tdJaBatido.textContent = guardarValor[4]
-    tdPropietario.textContent = guardarValor[5]
 
-    // cria uma classe tabela-resposta no td(para poder inserir o css que criei com esse mesmo nome)
-    tdMod.className = "tabela-resposta"
-    tdCor.className = "tabela-resposta"
-    tdMarca.className = "tabela-resposta"
-    tdAno.className = "tabela-resposta"
-    tdJaBatido.className = "tabela-resposta"
-    tdPropietario.className = "tabela-resposta"
-    // insere o td criado dentro do tr (basicamente fala que o td e o filho do tr e o td e pra ser inserido dentro do tr)
-    tabelaTr.append(tdMod, tdCor, tdMarca, tdAno, tdJaBatido, tdPropietario)
+
+    tabelaTd.tdMod.textContent = guardarValor.modelo
+    tabelaTd.tdCor.textContent = guardarValor.cor
+    tabelaTd.tdMarca.textContent = guardarValor.marca
+    tabelaTd.tdAno.textContent = guardarValor.ano
+    tabelaTd.tdJaBatido.textContent = guardarValor.jaBatido
+    tabelaTd.tdPropietario.textContent = guardarValor.nomePropietario
+
+    // cria uma classe tabela-resposta no td
+
+    tabelaTd.tdMod.className = "tabela-resposta"
+    tabelaTd.tdCor.className = "tabela-resposta"
+    tabelaTd.tdMarca.className = "tabela-resposta"
+    tabelaTd.tdAno.className = "tabela-resposta"
+    tabelaTd.tdJaBatido.className = "tabela-resposta"
+    tabelaTd.tdPropietario.className = "tabela-resposta"
+    // insere o td criado dentro do tr
+    tabelaTr.append(tabelaTd.tdMod,tabelaTd.tdCor,tabelaTd.tdMarca,tabelaTd.tdAno,tabelaTd.tdJaBatido,tabelaTd.tdPropietario)
     tBoddy.appendChild(tabelaTr)
 }
 
@@ -92,26 +78,24 @@ function validar() {
 // excluir tabela
 function excluir() {
     // cria o elemento com o id table-boddy
-    let tBoddy = document.getElementById('table-boddy')
+    const tBoddy = document.getElementById('table-boddy')
     // pega a classe de nome table-tr
-    let tr = document.querySelector('.tabela-tr')
+    const tr = document.querySelector('.tabela-tr')
     // pega a classe com o nome tabela-resposta
-    let td = document.querySelector('.tabela-resposta')
+    const td = document.querySelector('.tabela-resposta')
    // remove o filho de tr
     tr.removeChild(td)
    // remove o filho do tboddy
     tBoddy.removeChild(tr)
     // cria a variavel pra subtrair o total
-    let count = document.getElementById('contar')
+    const count = document.getElementById('contar')
 // subtrai 1 e altera o valor de conunt
     count.textContent--
     // verifica se o total esta igual a zero e esconde o botão de excluir
     if (count.textContent === '0'){
-        let btnApagar = document.getElementById('excluir-tabela')
+        const btnApagar = document.getElementById('excluir-tabela')
         // muda o botão que inicialmente e inline para none
         // isso faz o boão sumir novamente
         btnApagar.style.display = 'none'
     }
-
-
 }
